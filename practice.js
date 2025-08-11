130a11e5aaa5505462e4a7298d763f24679d1648
muhibmannan <129864845+muhibmannan@users.noreply.github.com>
2025-08-11 11:23:49 +1000

    Added JS variebles practice

---
+++ practice.js
@@ -0,0 +1,12 @@
+let name = 'Muhib';
+console.log(name);
+
+let interestRate = 0.3;
+console.log(interestRate);
+
+let firstName = 'Muhib';
+let lastName = 'Mannan';
+console.log(firstName + ' ' + lastName);
+console.log(`Hello, my name is ${firstName} ${lastName}`);
+
+
